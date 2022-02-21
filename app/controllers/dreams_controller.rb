8cbe0c4ff5c06a054f934f8ca4387cec668c4a3c
Nicole Czemerinski <nickyczeme@gmail.com>
2022-02-21 17:11:55 +0100

    link between homepage and index

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -2,9 +2,10 @@ class DreamsController < ApplicationController
   def index
      # mode_id = params[:id]
      #params[:mode]
-    @dreams = Dream.all
+     if params[:mode].present?
+      @dreams = Dream.where(mode: params[:mode])
+     else
+      @dreams = Dream.all
+     end
   end
-
-
-
 end
