55c9963d69c3ba169543b88fb151cfa95e54160c
sol <sbuker@itba.edu.ar>
2022-02-22 10:19:51 +0100

    gem

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,11 +1,11 @@
 class DreamsController < ApplicationController
   def index
-     # mode_id = params[:id]
-     #params[:mode]
-     if params[:mode].present?
-      @dreams = Dream.where(mode: params[:mode])
-     else
+    # mode_id = params[:id]
+    # params[:mode]
+    if params[:mode].present?
+      @dreams = Dream.all.where(mode: params[:mode])
+    else
       @dreams = Dream.all
-     end
+    end
   end
 end
