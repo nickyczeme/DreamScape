6938cad5d784806ee604888a04faecde8b7eb021
sol <sbuker@itba.edu.ar>
2022-02-24 11:47:36 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :dreams  do
+  resources :dreams do
     resources :bookings, only: [:new, :create, :show]
   end
+  get 'dashboard', to: 'pages#dashboard'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
